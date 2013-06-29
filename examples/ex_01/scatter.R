553b313cb3f2a5445d15d69a402ee8071d9431ce
Liang Bo Wang <ccwang002@gmail.com>
2013-06-29 22:58:05 +0800

    [slide] uniform quick code comments

--- examples/ex_01/scatter.R
+++ examples/ex_01/scatter.R
@@ -1,10 +1,10 @@
 # always load ggplot2 library first
 library(ggplot2)
 
-# basic
+# === basic ===
 plot(mtcars$wt, mtcars$mpg)
-# qplot()
+# === qplot() ===
 qplot(mtcars$wt, mtcars$mpg)
 qplot(wt, mpg, data=mtcars)  # if they are in same data.frame
-# ggplot()
+# === ggplot() ===
 ggplot(mtcars, aes(x=wt, y=mpg)) + geom_point()
