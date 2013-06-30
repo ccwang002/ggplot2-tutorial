b4a0ac52245ca50c7b47d3a4c3fa5d16d0113dc0
Liang Bo Wang <ccwang002@gmail.com>
2013-06-30 11:44:27 +0800

    [slide] add TWPT dataset

---
+++ examples/read_csv.R
@@ -0,0 +1,13 @@
+# parse outsource csv files
+
+# === input: twPT2013.csv ===
+# one can skim over the csv content by `$ head twPT2013.csv`
+# it contains comments starting with '#'
+twPT2013 <- read.csv("twPT2013.csv", 
+                     stringsAsFactors=FALSE,
+                     comment.char='#')
+# specify column names
+namelist <- c('schoolyear', 'year', 0:75)
+# save
+names(twPT2013) <- namelist
+save(list='twPT2013', file='twPT2013.RData')
