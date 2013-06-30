5238dae80dcd2bffda3f71629bc878054fb41f49
Liang Bo Wang <ccwang002@gmail.com>
2013-06-30 16:25:51 +0800

    add new dataset twCollegeDist2013

--- examples/read_csv.R
+++ examples/read_csv.R
@@ -11,3 +11,9 @@ namelist <- c('schoolyear', 'year', 0:75)
 # save
 names(twPT2013) <- namelist
 save(list='twPT2013', file='twPT2013.RData')
+
+# === input: twCollegeDist2013.csv ===
+twCollegeDist2013 <- read.csv('twCollegeDist2013.csv',
+                              stringsAsFactors=FALSE, 
+                              comment.char='#')
+save(list='twCollegeDist2013', file='twCollegeDist2013.RData')
