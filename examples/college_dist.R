bc7e2253d5365c849e56a621a8e489c67ebde62d
Liang Bo Wang <ccwang002@gmail.com>
2013-07-01 18:29:16 +0800

    add real data example

--- examples/college_dist.R
+++ examples/college_dist.R
@@ -13,6 +13,31 @@ g + geom_bar(stat='identity', width=0.8,
              position=position_dodge(0.7))
 
 # stack
-g + geom_bar(stat='identity', color='black') + 
+gs <- g + geom_bar(stat='identity', color='black') + 
   scale_fill_brewer(palette='Pastel1')
+gs
 
+# if we want to label the count of each category
+# directly set the position y of the label will fail
+# since they have been stacked
+gs + geom_text(aes(y=count, label=count))
+
+twCD <- ddply(twCD, "schoolyear", transform, 
+              label_y=cumsum(count) - 0.5 * count)
+
+ggplot(twCD, aes(x=schoolyear, y=count, fill=category)) +
+  geom_bar(stat='identity', color='black') + 
+  scale_fill_brewer(palette='Pastel1') + 
+  geom_text(aes(y=label_y, label=count), color='black', size=5)
+
+
+# === Proportional Stacked Plot ===
+twCD <- ddply(twCD, "schoolyear", transform,
+              percent_count = count / sum(count) * 100)
+
+percent_g <- ggplot(twCD, aes(x=schoolyear, y=percent_count, fill=category)) +
+  geom_bar(stat='identity', color='black') + 
+  scale_fill_brewer(palette='Set3')
+
+percent_g + scale_x_continuous(breaks=c(92, 98, 101), 
+                               labels=c('Elder', 'Mine', 'Younger'))
