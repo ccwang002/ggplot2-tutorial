8902f64cc9a502d16185d993a425cdc3b77d956c
Liang Bo Wang <ccwang002@gmail.com>
2013-12-04 02:29:40 +0800

    update content, finish basic plot

--- examples/play_data_process/play1.R
+++ examples/play_data_process/play1.R
@@ -1,16 +1,28 @@
 # source('read_raw.R')
 
-df <- read.csv('NTU_B02new_sexratio.csv')
-df.ntu_sexratio[, c("女", "男")]
-df.ntu_sexratio[df.ntu_sexratio$女 > 30, ]
-df.ntu_sexratio[
-  df.ntu_sexratio$女 > 30 & 
-  df.ntu_sexratio$男 < 20, 
+df.ntu <- read.csv('NTU_B02new_sexratio.csv')
+df.ntu[, c("女", "男")]
+df.ntu[df.ntu$女 > 30, ]
+df.ntu[
+  df.ntu$女 > 30 & 
+    df.ntu$男 < 20, 
 ]
 
 # get a quick summary of the data
-summary(df.ntu_sexratio)
+summary(df.ntu)
+ncol(df.ntu)
+nrow(df.ntu)
 
 library(ggplot2)
-ggplot(aes(x=df.ntu_sexratio[[1]], y=df.ntu_sexratio[[2]]), data=df.ntu_sexratio) + geom_bar(stat='identity') + 
+ggplot(aes(x=df.ntu[[1]], y=df.ntu[[2]]), data=df.ntu) + 
+  geom_bar(stat='identity') + 
   theme(text=element_text(family="Heiti TC Medium", size=10))
+
+colnames(df.ntu) <- c("disp", "total", "male", "female", "ratio")
+ggplot(df.ntu[c(1, 10, 20, 30, 40, 50, 56), ], aes(x=disp, y=total)) + 
+  geom_bar(stat='identity') + 
+  theme(text=element_text(family="Heiti TC Medium", size=10))
+
+ggplot(df.ntu, aes(x=female, y=male, color=disp)) + 
+  geom_point(size=5, show_guide=FALSE)
+
